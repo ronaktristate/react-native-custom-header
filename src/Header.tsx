368436dba31c970fe2bd45adacd5bb85cef068e0
Ronak.tristate <ronak.kotak@tristatetechnology.co.in>
2021-12-03 15:55:05 +0530

    added search feature

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, Image, Platform, ImageProps } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image, Platform, ImageProps, TextInput } from 'react-native';
 import { getStatusBarHeight } from './iPhoneHelp';
 
 export type Props = {
@@ -13,7 +13,11 @@ export type Props = {
    mainStyle: any,
    middleTextStyle: any,
    onPressRightIcon: any,
-   rightImageStyle: any
+   rightImageStyle: any,
+   showSearchBar: boolean,
+   searchValue: string,
+   onPressSearchCross: any,
+   onChangeText: any,
 };
 
 const Header: React.FC<Props> = ({
@@ -27,58 +31,82 @@ const Header: React.FC<Props> = ({
    mainStyle,
    middleTextStyle,
    onPressRightIcon,
-   rightImageStyle
+   rightImageStyle,
+   showSearchBar,
+   searchValue,
+   onPressSearchCross,
+   onChangeText,
 }) => {
    return (
       <View style={[styles.container, mainStyle,]} >
-         <View style={styles.firstView}>
-            {
-               backButton ?
-                  <View
-                     style={[styles.backButton]}
-                  >
-                     <TouchableOpacity
-                        onPress={() => { if (onBackButtonPress) onBackButtonPress(); }}
-                        delayPressIn={0}
-                     >
-                        <Image source={require("./assets/back-icon.png")} />
-                     </TouchableOpacity>
+         {
+            showSearchBar ?
+               <View style={[styles.middleSearch, {}]}>
+                  <TextInput
+                     placeholder={"Search.."}
+                     placeholderTextColor={"gray"}
+                     selectionColor={"gray"}
+                     onChangeText={onChangeText}
+                     value={searchValue}
+                     autoFocus
+                     style={styles.searchtext}
+                  />
+                  <TouchableOpacity onPress={onPressSearchCross}>
+                     <Image source={require("./assets/close-icon.png")} />
+                  </TouchableOpacity>
+               </View>
+               :
+               <React.Fragment>
+                  <View style={styles.firstView}>
+                     {
+                        backButton ?
+                           <View
+                              style={[styles.backButton]}
+                           >
+                              <TouchableOpacity
+                                 onPress={() => { if (onBackButtonPress) onBackButtonPress(); }}
+                                 delayPressIn={0}
+                              >
+                                 <Image source={require("./assets/back-icon.png")} />
+                              </TouchableOpacity>
+                           </View>
+                           :
+                           leftIcon ?
+                              <View
+                                 style={[styles.backButton]}
+                              >
+                                 <TouchableOpacity
+                                    onPress={() => leftIconPress && leftIconPress()}
+                                    delayPressIn={0}
+                                    hitSlop={{ top: 10, left: 10, right: 10, bottom: 10 }}
+                                 >
+                                    <Image source={leftIcon.source} />
+                                 </TouchableOpacity>
+                              </View>
+                              :
+                              null
+                     }
                   </View>
-                  :
-                  leftIcon ?
-                     <View
-                        style={[styles.backButton]}
-                     >
-                        <TouchableOpacity
-                           onPress={() => leftIconPress && leftIconPress()}
-                           delayPressIn={0}
-                           hitSlop={{ top: 10, left: 10, right: 10, bottom: 10 }}
-                        >
-                           <Image source={leftIcon.source} />
+                  <View
+                     style={[styles.middleView]}>
+                     {middleText ?
+                        <Text numberOfLines={1} style={[styles.middleTextStyle, middleTextStyle, {}]}> {middleText}</Text>
+                        :
+                        null}
+                  </View>
+                  {rightIcon ?
+                     <View style={[styles.lastView]}>
+                        <TouchableOpacity activeOpacity={0.8} onPress={onPressRightIcon}>
+                           {rightImage ?
+                              <Image source={rightImage} style={rightImageStyle} />
+                              : null
+                           }
                         </TouchableOpacity>
                      </View>
                      :
-                     null
-            }
-         </View>
-         <View
-            style={[styles.middleView]}>
-            {middleText ?
-               <Text numberOfLines={1} style={[styles.middleTextStyle, middleTextStyle, {}]}> {middleText}</Text>
-               :
-               null}
-         </View>
-         {rightIcon ?
-            <View style={[styles.lastView]}>
-               <TouchableOpacity activeOpacity={0.8} onPress={onPressRightIcon}>
-                  {rightImage ?
-                     <Image source={rightImage} style={rightImageStyle} />
-                     : null
+                     <View style={[styles.lastView]} />
                   }
-               </TouchableOpacity>
-            </View>
-            :
-            <View style={[styles.lastView]} />
+               </React.Fragment>
          }
       </View>
    )
@@ -115,5 +143,23 @@ const styles = StyleSheet.create({
       paddingRight: 20,
       flex: 0.3
    },
+   middleSearch: {
+      flex: 1,
+      borderWidth: 1,
+      marginHorizontal: 20,
+      paddingHorizontal: 10,
+      borderColor: 'white',
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      borderRadius: 10,
+      paddingVertical: 10
+   },
+   searchtext: {
+      color: 'black',
+      fontSize: 14,
+      flex: 1,
+      height: 50
+   }
 })
 
